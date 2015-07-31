ea24290d1ca2e8e50670cd942950db40b88e2917
chenxiu <chenxiu@chenxi.local>
2015-07-31 01:13:51 -0400

    mobile front end

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,22 @@
 
-    var video= document.getElementById('vedio');
-    video.playbackRate = 0.5;
+    // var video= document.getElementById('vedio');
+    // video.playbackRate = 0.5;
+
+	var menupush = function() {
+	  /* Push the body and the nav over by 285px over */
+	  $('.icon-menu').click(function() {
+	    $('#nav').animate({
+	      left: "0%"
+	    }, 200);
+	  });
+
+	  /* Then push them back */
+	  $('.icon-close').click(function() {
+	    $('#nav').animate({
+	      left: "-30%"
+	    }, 200);
+
+	  });
+	};
+
+	$(document).ready(menupush);
